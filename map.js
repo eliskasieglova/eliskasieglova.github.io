430ad0e653a2cc3352805dfa0bfc6585ae6c096f
eliskasieglova <57137417+eliskasieglova@users.noreply.github.com>
2024-06-10 18:13:14 +0200

    Update map.js

--- map.js
+++ map.js
@@ -95,7 +95,6 @@ L.control.layers(null, overlayMaps).addTo(map);
 function resetHighlight(e) {
     layer2023.resetStyle(e.target);
     info.update();
-    plot.showimg();
 };
 
 function zoomToFeature(e) {
